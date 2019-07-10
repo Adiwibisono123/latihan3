8431b29da30c26dadb9e6d19bcffebca2e48d426
Adiwibisono123 <adiwibi03@gmail.com>
2019-07-10 20:43:11 +0700

    Laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <img src="https://hpsaja.com/wp-content/uploads/2015/08/xiomi-logo.jpg" height="35">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="/home">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/about">about</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/contact">contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="jumbotron bg-warning">
+  <h1 class="display-4">Xiaomi global community</h1>
+  <p class="lead"> Xiaomi adalah perusahaan elektronik dan software yang bertempat di Beijing, Cina. Xiaomi mendesain, mengembangkan, dan menjual smartphone, aplikasi ponsel, laptop dan produk lain yang berhubungan dengan elektronik.
+
+Xiaomi dimiliki oleh swasta milik pribadi seorang berasal dari Tiogkok dan berkantor pusat di Beijing Cina. Berdiri pada 2010 Xiaomi menjadi perusahaan besar dan menjadi salah satu perusahaan terkemuka di Tiongkok. Bahkan Xiaomi menjadi produsen paling laris ke tiga di dunia dalam penjualan smartphone. Berawal pada agustus 2011 awal Xiaomi merilis ponsel cerdas petamanya yang mendapat respon bagus di daratan Tiongkok Xiaomi yang dipimpin oleh Lei Jun memperluas pangsa pasar ke luar daratan Tiongkok. Lei Jun sendiri termasuk dalam orang ke 23 terkaya di Tiongkok menurut Forbes. Perusahaannya kemudian berkembang pesat hingga sekarang memiliki saham lebih dari 10 miliar USD dan memiliki karyawan lebih dari 5000 karyawan dari berbagai negara.
+
+Pengambilan nama Xiaomi sendiri berasal dari Bahasa Mandarin yang memiliki arti Beras Kecil. Menurut Lei Jun sendiri nama Xiaomi memiliki arti sangat besar. Xiao berarti sebutir beras dalam konsep Buddha sama besarnya seperti Gunung dan Mi sendiri singkatan dari Mobile Internet atau juga bisa disebut Mission Imposibble karena pendirinya sendiri mendapatkan banyak masalah ketika mendirikan Xiaomi hingga terlihat mustahil untuk diselesaikan.</p>
+  <hr class="my-4">
+  <p>Pelajari selengkapnya tentang ponsel kami</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <img src="https://hpsaja.com/wp-content/uploads/2015/08/xiomi-logo.jpg" height="35">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="/home">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/about">about</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/contact">contact</a>
+      </li>
+    </ul>
+  </div>
+ 
+  <div class="clearfix">
+  <button type="button" class="btn btn-outline-primary">Indonesia</button>
+  <button type="button" class="btn btn-outline-danger">Lainnya</button>
+</div>
+</nav>
+
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="http://uf.cari.com.my/forumx/mforum/forum/201612/19/175907wg1kg1r616ojirvl.jpg" class="d-block w-100" alt="..." height="600" width="500">
+    </div>
+    <div class="carousel-item">
+      <img src="https://2.bp.blogspot.com/-8175ZtERZ9k/WoGgE-UUrwI/AAAAAAAAAcQ/nTYZivio_NMwXFrv3Zz1CjiB3xq8ziyOgCLcBGAs/s1600/Xiomi-redmi.png" class="d-block w-100" alt="..." height="600" width="500">
+    </div>
+    <div class="carousel-item">
+      <img src="https://mostarski.ba/wp-content/uploads/2017/01/iphone-7.jpg" class="d-block w-100" alt="..." height="600" width="500">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<center>
+    <h1>Tentang Ponsel</h1>
+</center>
+<div class="">
+    <div class="jumbotron bg-warning">
+  <div class="row">
+    <div class="col-sm">
+    <div class="card" style="width: 18rem;">
+  <img src="https://www.santridrajat.com/wp-content/uploads/2018/04/Xiaomi-Redmi-5-HP-Android-Murah-Keren-Dan-Berkualitas-1.jpg" class="card-img-top" alt="..." height="300">
+  <div class="card-body">
+    <h5 class="card-title">Redmi 7a</h5>
+
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm">
+    <div class="card" style="width: 18rem;">
+  <img src="https://1.bp.blogspot.com/-hgmUHDb6qMM/VxJ8f0RAPTI/AAAAAAAAD6o/pIplJqLOTKQJhzYJjegS7iM-bcsZueUgQCLcB/s1600/xiaomi-redmi-3-pro.jpg" class="card-img-top" alt="..." height="300">
+  <div class="card-body">
+    <h5 class="card-title">Redmi 6z</h5>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+    <div class="col-sm">
+    <div class="card" style="width: 18rem;">
+  <img src="https://s2.bukalapak.com/img/2157454862/large/Terbaru_Xiaomi_S2.jpg" class="card-img-top" alt="..." height="300">
+  <div class="card-body">
+    <h5 class="card-title">Redmi note 5/h5>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+</div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
